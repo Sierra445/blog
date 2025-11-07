a2ffd7b70b9a13f8c40c8a2b54939adacb0bb5de
Elsie <sierranhlanhla@gmail.com>
2025-11-07 07:06:40 +0200

    Add add_post view for creating new blog posts

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import Post
 # Create your views here.
+
+def index(request):
+    post = Post.objects.order_by('-created_at')
+    return render(request, 'blog_app/index.html', {'posts': post})
+
+def add_post(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        Post.objects.create(title=title, content=content)
+        return redirect('index')
+    return render(request, 'blog_app/add_post.html')
+
