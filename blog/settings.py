4f6b5668bd989e2eb45fb15d9fb5b134da38c80e
elmsizacjc025 <elmsizacjc025@student.wethinkcode.co.za>
2025-11-13 11:37:35 +0200

    fixed the blog

--- blog/settings.py
+++ blog/settings.py
@@ -122,3 +122,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# Only needed in development to serve static files
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
